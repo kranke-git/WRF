d5000b548d3f80337349914f2149605908abeee7
kranke <pgiani@nd.edu>
2023-06-01 16:22:21 -0400

    Minor mods -- Added ww in Registry

--- configure_and_compile.sh
+++ configure_and_compile.sh
@@ -7,7 +7,7 @@ module load intel
 module load mvapich2/2.3.1   # This is for InfiniBand and MPI; should load intel too
 module unload R
 
-EXENAME='REAL_WRF_WITH_TENDENCIES'
+EXENAME='FLAT_EMHILL_PERTURB_T'
 
 export NETCDF='/afs/crc/group/EnvStat/build'
 export WRFIO_NCD_LARGE_FILE_SUPPORT=1
@@ -17,13 +17,13 @@ export LIBTIFF='/afs/crc/group/EnvStat/build/lib'
 export GEOTIFF='/afs/crc/group/EnvStat/build/lib'
 
 #./configure
- echo 15 | ./configure #Intel (dmpar) - MPI
+# echo 15 | ./configure #Intel (dmpar) - MPI
 # echo 14 | ./configure #Intel (smpar)   - openmp
-#echo 13 | ./configure   #Intel (serial)
-./compile em_real
-#./compile em_hill2d_x
+echo 13 | ./configure   #Intel (serial)
+#./compile em_real
+./compile em_hill2d_x
 
 mkdir                 main/executables/${EXENAME}
-#cp -p main/ideal.exe  main/executables/${EXENAME}
-cp -p main/real.exe   main/executables/${EXENAME}
+cp -p main/ideal.exe  main/executables/${EXENAME}
+#cp -p main/real.exe   main/executables/${EXENAME}
 cp -p main/wrf.exe    main/executables/${EXENAME}
