f0c835b336cf64e282cf27f1f1417d5372a697bc
kranke <pgiani@nd.edu>
2023-09-28 17:08:07 -0400

    Implemented acoustic tendencies and moisture advection

--- configure_and_compile.sh
+++ configure_and_compile.sh
@@ -3,11 +3,10 @@
 # IMPORTANT: DO NOT LOAD NETCDF LIBRARIES
 # USE PRIVATE ONES BY SETTING NETCDF ENV VARIABLE
 
-module load intel
-module load mvapich2/2.3.1   # This is for InfiniBand and MPI; should load intel too
-module unload R
+# module load mvapich2/2.3.1   # This is for InfiniBand and MPI; should load intel too
+module load mpich
 
-EXENAME='FLAT_EMHILL_PERTURB_T_2D'
+EXENAME='EM_IDEAL_MPICH_4.5.1_WITH_TENDENCIES_NOT_IN_RESTART_QTEND'
 
 export NETCDF='/afs/crc/group/EnvStat/build'
 export WRFIO_NCD_LARGE_FILE_SUPPORT=1
@@ -17,14 +16,17 @@ export LIBTIFF='/afs/crc/group/EnvStat/build/lib'
 export GEOTIFF='/afs/crc/group/EnvStat/build/lib'
 
 #./configure
-# echo 15 | ./configure #Intel (dmpar) - MPI
+ echo 15 | ./configure #Intel (dmpar) - MPI
 # echo 14 | ./configure #Intel (smpar)   - openmp
-echo 13 | ./configure   #Intel (serial)
+# echo 13 | ./configure   #Intel (serial)
+./compile em_les
 #./compile em_real
-./compile em_hill2d_x
 
 mkdir                                  main/executables/${EXENAME}
 cp -p main/ideal.exe                   main/executables/${EXENAME}
-#cp -p main/real.exe   main/executables/${EXENAME}
+#cp -p main/real.exe                    main/executables/${EXENAME}
 cp -p main/wrf.exe                     main/executables/${EXENAME}
 cp -p dyn_em/module_initialize_ideal.F main/executables/${EXENAME}
+cp -p configure_and_compile.sh         main/executables/${EXENAME} 
+
+
